8bb0f299589ea90c2755252034d310bf715884f3
0xFEEDC0DE64 <daniel@brunner.ninja>
2022-05-23 23:58:16 +0200

    add missing c++ guard

--- cores/esp32/esp32-hal-misc.h
+++ cores/esp32/esp32-hal-misc.h
@@ -2,7 +2,15 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void yield(void);
 
 unsigned long micros();
 void delayMicroseconds(uint32_t us);
+
+#ifdef __cplusplus
+}
+#endif
